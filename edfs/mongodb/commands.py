31b1a8ffc774e280bcc8fc121b3ce74849bed66b
tiffany wong <61953784+tifftiffany@users.noreply.github.com>
2022-10-30 04:15:43 -0700

    Update commands.py
    MongoDB_EDFS: add mkdir, ls function

--- edfs/mongodb/commands.py
+++ edfs/mongodb/commands.py
@@ -1,30 +1,84 @@
-def mkdir(path: str): 
-    # create a directory in file system, e.g., mkdir /user/john
-    pass
+import pymongo
+from pymongo import MongoClient
+
+from creds import FB_BASE_URL
+
+client = MongoClient('localhost', 27017)
+db = client['test']
 
 
-'''
-Example:
+def mkdir(path:str):
+    
+    # need to add: check if the directory exist and add to existing record if so
 
-ls('user/home') --> 
-    tmp = data_from_home_in_fb
-    for f in tmp.keys():
-        print(f)
+    if '.' in path:
+        return ('INVALID PATH')
+    else:
+        path = preprocess_path(path)
+
+    path_split = [s for s in path.split('/') if s != '']
+    
+    # collection - namenode
+    collection = db[path_split[0]]
+    
+    # create entries for directories      
+    namenode_dict = {'root': {}}
+    cur_dict = namenode_dict
+    for s in path.split('/')[1:]:
+        if s in cur_dict.keys():
+            cur_dict = cur_dict[s]
+        else:
+            cur_dict[s] = {}
+            cur_dict = cur_dict[s]
+    
+    # check if path exist: (i). already exist (ii). partly exist, need to insert new data (iii). not exist
+    path_exist = False
+    for document in cursor:
+        count = -1
+        cur_dict = document
+        for s in path_split[1:]:
+            if path_exist:
+                break
+            if s in cur_dict:
+                count += 1
+                cur_dict = cur_dict[s]
+                
+                # for (i)
+                if count == len(path_split[2:]):
+                    path_exist = True
+                    print('ALREADY EXIST')
+                    break
+    
+    return collection.insert_one(namenode_dict)
 
 
-currently in firebase:
-    root: {
-        user: {
-            home: {
-                file1: {}
-                file2: {}
-            }
-        }
-}
-'''
-def ls(path: str='.'): 
+def ls(path: str): 
+    
     # listing content of a given directory, e.g., ls /user
-    pass
+
+    if '.' in path:
+        return 'NOT A DIRECTORY'
+    
+    path = preprocess_path(path)
+    path_split = [s for s in path.split('/') if s != '']
+    collection = db[path_split[0]]
+    cursor = collection.find({})
+    for document in cursor:
+        exist = True
+        cur_dict = document
+        for s in path_split[1:]:
+            if s in cur_dict:
+                cur_dict = cur_dict[s]
+                keys = list(cur_dict.keys())
+            else:
+                exist = False
+                break
+        if exist:
+            return '\n'.join(keys)
+            break
+
+    if not exist:
+        return ('NOT A DIRECTORY')
 
 
 def cat(path: str): 
