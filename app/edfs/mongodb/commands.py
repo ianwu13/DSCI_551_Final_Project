df447420b88a618c813e75e2aba049e6d0e6ee81
tifftiffany <twong225@usc.edu>
2022-11-14 22:12:55 -0800

    mongodb: add preprocess_path, mkdir, ls function

--- app/edfs/mongodb/commands.py
+++ app/edfs/mongodb/commands.py
@@ -1,7 +1,50 @@
-def mkdir(path: str): 
-    # create a directory in file system, e.g., mkdir /user/john
-    pass
+import pymongo
+from pymongo import MongoClient
 
+client = MongoClient('localhost', 27017)
+db = client['test']
+collection = db['namenode']
+
+def preprocess_path(path: str):
+    if path == '.':
+        return 'namenode/root/'
+    elif path[-1] == '/':
+        path = path[:-1]
+    if path[:6] == '/root/':
+        path = path[6:]
+    elif path[0] == '/':
+        path = path[1:]
+    return ''.join(['namenode/root/', path])
+
+def mkdir(path:str):
+    
+    if '.' in path:
+        return ('INVALID PATH')
+    else:
+        path = preprocess_path(path)
+
+    path_split = [s for s in path.split('/') if s != '']
+
+    # check if path exists:
+    new_path = []
+    exists = False
+    for i in range(2, len(path_split)+1):
+        check_path = '.'.join(path_split[1:i])
+        if list(db.namenode.find({check_path: {'$exists': True}})) == []:
+            exists = False
+            new_path.append(check_path)
+        else: 
+            exists = True
+    if exists:
+        return ('Path already exist')
+    
+    # create record for new path
+    else:
+        for newpath in new_path:
+            db.namenode.update_one({newpath.rsplit('.', 1)[0]: {'$exists': True}}, 
+                                   {'$set': {newpath: {}}})
+
+    return ('Path Created')
 
 '''
 Example:
@@ -22,9 +65,38 @@ currently in firebase:
         }
 }
 '''
-def ls(path: str='.'): 
+
+def ls(path: str): 
+    
     # listing content of a given directory, e.g., ls /user
-    pass
+
+    if '.' in path:
+        return 'NOT A DIRECTORY'
+    
+    path = preprocess_path(path)
+    path_split = [s for s in path.split('/') if s != '']
+    collection = db['namenode']
+    cursor = collection.find({})
+    
+    for document in cursor:
+        exist = True
+        cur_dict = document
+        for s in path_split[1:]:
+            if s in cur_dict:
+                cur_dict = cur_dict[s]
+                keys = list(cur_dict.keys())
+            else:
+                exist = False
+                break
+        if exist:
+            if keys == [ ]:
+                return
+            else:
+                return ', '.join(keys)
+            break
+
+    if not exist:
+        return ('NOT A DIRECTORY')
 
 
 def cat(path: str): 
